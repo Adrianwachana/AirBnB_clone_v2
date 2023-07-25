542f552a7a3f529764e142a3bcdb6db8058c56b9
Adrianwachana <adrianwafula@gmail.com>
2023-07-25 17:40:09 +0000

    updated

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 # starts flask
 from flask import Flask
+
 app = Flask(__name__)
 
-
+# Define the route for "/"
 @app.route('/', strict_slashes=False)
-def hello_world():
-    # prints hello
-    return "Hello HBNB!"
+def hello_hbnb():
+    return 'Hello HBNB!'
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
