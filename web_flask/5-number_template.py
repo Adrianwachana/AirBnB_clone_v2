a63981b082e036ed208aadb84e12024ef599d95d
Adrianwachana <adrianwafula@gmail.com>
2023-07-26 17:48:00 +0000

    5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# starts flask with c thing
+"""
+This module defines a Flask web application.
+"""
 from flask import Flask, abort, render_template
 
 app = Flask(__name__)
@@ -7,47 +9,55 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    # hello
+    """
+    Display "Hello HBNB!" on the root path.
+    """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    # hbnb
+    """
+    Display "HBNB" on the path "/hbnb".
+    """
     return 'HBNB'
 
 
 @app.route('/c/<text>')
 def ctext(text):
-    # c text
+    """
+    Display "C ", followed by the value of the text variable.
+    Replace underscore (_) symbols with a space.
+    """
     return 'C ' + text.replace("_", ' ')
 
 
 @app.route('/python/')
 @app.route('/python/<text>')
 def ptext(text='is cool'):
-    # python text
+    """
+    Display "Python ", followed by the value of the text variable (default is 'is cool').
+    Replace underscore (_) symbols with a space.
+    """
     return 'Python {}'.format(text.replace("_", ' '))
 
 
-@app.route('/number/<n>')
+@app.route('/number/<int:n>')
 def ntext(n):
-    # number
-    try:
-        return '{} is a number'.format(int(n))
-    except:
-        abort(404)
+    """
+    Display "{} is a number" only if n is an integer.
+    """
+    return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<n>')
-def temptext(n):
-    # text with jinja2
-    try:
-        return render_template('5-number.html', n=int(n))
-    except:
-        abort(404)
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """
+    Display an HTML page only if n is an integer.
+    """
+    return render_template('5-number_template.html', n=n)
 
 
-if __name__ == "__main__":
-    app.url_map.strict_slashes = False
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
