f094ff7766767a7a1e922af983795224b464ea82
Adrianwachana <adrianwafula403@gmail.com>
2023-07-26 20:17:53 +0300

    new line

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,12 +6,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
     Display "Hello HBNB!" on the root path.
     """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
@@ -20,6 +22,7 @@ def hbnb():
     """
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """
@@ -27,6 +30,7 @@ def c_is_fun(text):
     Replace underscore (_) symbols with a space.
     """
     return 'C {}'.format(text.replace('_', ' '))
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -37,6 +41,6 @@ def python_is_cool(text='is_cool'):
     """
     return 'Python {}'.format(text.replace('_', ' '))
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
